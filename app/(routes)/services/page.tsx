418d333d7e81dfa9654e015a0f8a6bda5385ce7d
Josebernalt <jbernaltambo@gmail.com>
2024-08-22 21:43:21 -0500

    second commit

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -13,7 +13,7 @@ const ServicesPage = () => {
             <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
                 <div className="max-w-[450px] mt-40 md:mt-0">
                     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
-                        My{" "}
+                        My 
                         <span className="font-bold text-secondary">
                             services.
                         </span>
@@ -22,8 +22,7 @@ const ServicesPage = () => {
                         I offer Full Stack web and application development
                         services, specialized in creating attractive and functional websites and applications.
                         I use the latest technologies, such as HTML, CSS, JavaScript, React, Next.js, Node.js,
-                        Bootstrap, Tailwind, and MySQL, to design intuitive and responsive user interfaces that
-                        reflect my clients' brand identity and enhance their presence online. Additionally, I have
+                        Bootstrap, Tailwind, and MySQL, to design intuitive and responsive. Additionally, I have
                         experience in JAVA, C#, and Git/GitHub, which allows me to manage projects efficiently and
                         collaborate effectively in development teams.
                     </p>
